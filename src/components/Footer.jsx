c6fd2ae00dd8e6a7015efd115b954a249ce557ea
cerberus8888 <v26396341@gmail.com>
2023-02-11 16:56:43 -0500

    Some css fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ export const Footer = () => {
         <a href='https://www.linkedin.com/in/luis-daniel-rojas-a071b8169/' target='_blank'><h1><i className="fab fa-linkedin"></i></h1></a>
         <a href='https://github.com/Ciberstein' target='_blank'><h1><i className="fab fa-github-square"></i></h1></a>
         <a href='https://cyberstein.netlify.app/' target='_blank'><h1><i class="fas fa-info-square"></i></h1></a>
-
     </footer>
   )
 }
